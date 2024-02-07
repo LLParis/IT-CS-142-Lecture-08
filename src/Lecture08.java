0347b5a6fcc924bd194d11f4844d5b68895916ba
Your Name <you@example.com>
2024-02-07 04:48:59 -0800

    Initial Commit

---
+++ src/Lecture08.java
@@ -0,0 +1,38 @@
+public class Lecture08 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #12 here:
+    // NOTE: FIRST try to predict what the output will be WITHOUT actually running the program
+    // write down your answer here
+    // input: 6.7         This file has several input lines.
+    // input: 10 20                 30 40
+    // input: test
+    // 3 total
+
+
+    // NEXT run the program and see the results (you can copy-and-paste it here)
+    // (I'm not including it in the starter project so that people don't just run
+    //  it as the first thing they do :)  )
+
+    // LASTLY compare the two results and if they're different go back and figure out
+    // why the computer's output is different from what you predicted.
+    // The computer's results are the same as I predicted.
+
+    // Put your answer for #13 here:
+    // (Do this like #5A - predict, write your prediction, run the code, then explain any differences)
+
+    // Put your answer for #14 here:
+
+    // Put your answer for #18 here:
+
+    // EXERCISES:
+
+    // Put your answer for #7 here:
+    // You will need to add the method in above main(), but then call it here
+  }
+}
